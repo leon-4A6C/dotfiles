9f45215cbc58491e7f86ced2e100420cde028518
leon in 't Veld <leon3110l@gmail.com>
2017-11-13 21:26:43 +0100

    added fish

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+# theme
+set theme_color_scheme solarized-light
+
+# android
+set --export ANDROID_HOME $HOME/Android/Sdk
+set --export PATH $PATH $ANDROID_HOME/tools
+set --export PATH $PATH $ANDROID_HOME/platform-tools
